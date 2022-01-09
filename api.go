701c0bfef399c774ebcdb09a758279cc5ccb596f
James Yeap <jamesyeap.ys@gmail.com>
2022-01-09 08:45:17 +0800

    Fix issue where invalid-requests are not stopped immediately.

--- api.go
+++ api.go
@@ -100,23 +100,20 @@ func main() {
 	r.Use(CORSMiddleware());
 
 	/* --------------------------------------------------------------- URL ENDPOINTS -------------- */
-
 	// ping test
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "Hello!")
 	})
 
 	// sign a new user up
-	r.POST("/signup", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/signup", func(c *gin.Context) {		
 		var params Credentials;
 		err := c.BindJSON(&params);
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(params.Password), 8)
 
-		details, err := signUp(params.Username, hashedPassword, c, cancel)
+		details, err := signUp(params.Username, hashedPassword, c)
 
 		if (err == nil) {
 			c.JSON(200, details);
@@ -125,15 +122,11 @@ func main() {
 
 	// log in an existing user
 	r.POST("/login", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
 		var params Credentials;
 		err := c.BindJSON(&params);
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);		
 
-		fmt.Println(params);
-
-		details, err := logIn(params.Username, params.Password, c, cancel)
+		details, err := logIn(params.Username, params.Password, c)
 
 		if (err == nil) {
 			c.JSON(200, details);
@@ -141,114 +134,102 @@ func main() {
 	})
 
 	// get tasks by category id and completion status
-	r.POST("/gettasks", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/gettasks", func(c *gin.Context) {		
 		var params QueryTasksParams
 		err := c.BindJSON(&params);
-		assertJSONSuccess(c, cancel, err)
+		assertJSONSuccess(c, err)
 
-		var taskList []Task = getTasks(params, c, cancel);
+		var taskList []Task = getTasks(params, c);
 
 		c.JSON(200, taskList)
 	})
 
 	// get a specific task by id
-	r.POST("/gettask", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/gettask", func(c *gin.Context) {		
 		var params GetTaskByIdParams;
 		err := c.BindJSON(&params);
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		var t Task = getTask(params.Id, c, cancel);
-
-		c.JSON(200, t)
+		var t Task = getTask(params.Id, c);
+		
+		if (!c.IsAborted()) {
+			c.JSON(200, t)
+		}
 	})
 
 	// update a specific task by id
-	r.POST("/updatetask", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/updatetask", func(c *gin.Context) {		
 		var params UpdateTaskParams
 		err := c.BindJSON(&params);
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		updateTask(params, c, cancel)
+		updateTask(params, c)
 
-		c.JSON(200, fmt.Sprintf("Successfully updated task with id: %v", params.Id))
+		if (!c.IsAborted()) {
+			c.JSON(200, fmt.Sprintf("Successfully updated task with id: %v", params.Id))
+		}
 	})
 
 	// mark a task as completed by id
-	r.POST("/completetask", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/completetask", func(c *gin.Context) {		
 		var params GetTaskByIdParams;
 		err := c.BindJSON(&params)
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		completeTask(params.Id, c, cancel);
+		completeTask(params.Id, c);
 
 		c.JSON(200, fmt.Sprintf("Successfully completed task with id: %v", params.Id))
 	})
 
 	// mark a task as incomplete by id
 	r.POST("/incompletetask", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
+		
 
 		var params GetTaskByIdParams;
 		err := c.BindJSON(&params)
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		incompleteTask(params.Id, c, cancel);
+		incompleteTask(params.Id, c);
 
 		c.JSON(200, fmt.Sprintf("Successfully marked task as incomplete with id: %v", params.Id))
 	})
 
 	// deletes a task by id
-	r.POST("/deletetask", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/deletetask", func(c *gin.Context) {		
 		var params GetTaskByIdParams;
 		err := c.BindJSON(&params)
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		deleteTask(params.Id, c, cancel);
+		deleteTask(params.Id, c);
 
 		c.String(200, fmt.Sprintf("Successfully deleted task with id: %v", params.Id))
 	})
 
 	// adds a task
-	r.POST("/addtask", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/addtask", func(c *gin.Context) {		
 		var params CreateTaskParams
 		err := c.BindJSON(&params)
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		addTask(params, c, cancel)		
+		addTask(params, c)		
 	})
 
 	// create a category
-	r.POST("/addcategory", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/addcategory", func(c *gin.Context) {		
 		var params CreateCategoryParams
 		err := c.BindJSON(&params)
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		addCategory(params, c, cancel)	
+		addCategory(params, c)	
 	})
 
 	// gets a list of all categories
-	r.POST("/allcategories", func(c *gin.Context) {
-		_, cancel := context.WithCancel(context.Background());
-
+	r.POST("/allcategories", func(c *gin.Context) {		
 		var params User;
 		err := c.BindJSON(&params);
-		assertJSONSuccess(c, cancel, err);
+		assertJSONSuccess(c, err);
 
-		var categoryList []Category = getAllCategories(params.Id, c, cancel);
+		var categoryList []Category = getAllCategories(params.Id, c);
 
 		c.JSON(200, categoryList)
 	})
@@ -259,21 +240,21 @@ func main() {
 
 /* ----------------------------------------------------------------- DATABASE FUNCTIONS --------- */
 /* Initialises and returns a connection to the database */
-func connectDB(client *gin.Context, cancel context.CancelFunc) (c *pgx.Conn) {
+func connectDB(client *gin.Context) (c *pgx.Conn) {
 	// load the .env file that contains postgresql connection details
 	godotenv.Load(".env")
 
 	// open a connection to the database
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	// check that the connection is successfully established
-	assertDBSuccess(client, cancel, err);
+	assertDBSuccess(client, err);
 
 	return conn;
 }
 
 /* Creates an account for a new user and returns the details of the user */
-func signUp(username string, password []byte, client *gin.Context, cancel context.CancelFunc) (User, error) {
-	c := connectDB(client, cancel)
+func signUp(username string, password []byte, client *gin.Context) (User, error) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	_, err := c.Exec(context.Background(), "INSERT INTO users (username, password) VALUES ($1, $2);", username, password);
@@ -285,9 +266,7 @@ func signUp(username string, password []byte, client *gin.Context, cancel contex
 		fmt.Fprintf(os.Stderr, "Unable to create a new user account: %v\n", err);
 
 		// return HTTP Error 409: Conflict
-		client.JSON(409, gin.H{"error": err.Error()});
-
-		cancel();
+		client.AbortWithStatusJSON(409, gin.H{"error": "Username already taken."});		
 
 		return user, err;
 	}
@@ -296,14 +275,14 @@ func signUp(username string, password []byte, client *gin.Context, cancel contex
 		&user.Id,
 		&user.Username,
 	)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 
 	return user, nil;
 }
 
 /* Logs in an existing-user */
-func logIn(username string, password string, client *gin.Context, cancel context.CancelFunc) (User, error) {
-	c := connectDB(client, cancel)
+func logIn(username string, password string, client *gin.Context) (User, error) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	var storedPassword string;
@@ -316,9 +295,7 @@ func logIn(username string, password string, client *gin.Context, cancel context
 		fmt.Fprintf(os.Stderr, "Unable to log user in: %v\n", err);
 
 		// return HTTP Error 401: Unauthorised
-		client.JSON(401, gin.H{"error": err.Error()});
-
-		cancel();
+		client.AbortWithStatusJSON(401, gin.H{"error": "Username doesn't exist."});		
 
 		return user, err;
 	}
@@ -328,9 +305,7 @@ func logIn(username string, password string, client *gin.Context, cancel context
 	if (err != nil) {
 		fmt.Fprintf(os.Stderr, "Unable to log user in: %v\n", err);
 
-		client.JSON(401, gin.H{"error": err.Error()});
-
-		cancel();
+		client.AbortWithStatusJSON(401, gin.H{"error": "Wrong password."});		
 
 		return user, err;
 	}
@@ -340,20 +315,18 @@ func logIn(username string, password string, client *gin.Context, cancel context
 		&user.Id,
 		&user.Username,
 	)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 
 	return user, nil;
 }
 
 /* Returns an array of Tasks based on filtering criteria */
-func getTasks(filterParams QueryTasksParams, client *gin.Context, cancel context.CancelFunc) ([]Task) {
-	c := connectDB(client, cancel)
+func getTasks(filterParams QueryTasksParams, client *gin.Context) ([]Task) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	var tasks pgx.Rows;
 	var err error;
-
-	fmt.Println(filterParams);
 
 	if (filterParams.CategoryId == -1) {
 		if (filterParams.CompletionStatus == 0) {
@@ -393,7 +366,7 @@ func getTasks(filterParams QueryTasksParams, client *gin.Context, cancel context
 			&t.Created_at,
 			&t.Updated_at,	
 		)
-		assertDBOperationSuccess(client, cancel, err);
+		assertDBOperationSuccess(client, err);
 		taskSlice = append(taskSlice, t)
 	}
 
@@ -401,8 +374,8 @@ func getTasks(filterParams QueryTasksParams, client *gin.Context, cancel context
 }
 
 /* Return a Task by its id */
-func getTask(task_id int, client *gin.Context, cancel context.CancelFunc) (Task) {
-	c := connectDB(client, cancel)
+func getTask(task_id int, client *gin.Context) (Task) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	var t Task
@@ -418,88 +391,88 @@ func getTask(task_id int, client *gin.Context, cancel context.CancelFunc) (Task)
 		&t.Created_at,
 		&t.Updated_at,		
 	)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 
 	return t;
 }
 
 /* Update a Task by its id */
-func updateTask(t UpdateTaskParams, client *gin.Context, cancel context.CancelFunc) {
-	c := connectDB(client, cancel)
+func updateTask(t UpdateTaskParams, client *gin.Context) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	_, err := c.Exec(context.Background(), "UPDATE tasks SET category_id=$1, title=$2, description=$3, deadline=$4 WHERE id=$5;", t.Category_Id, t.Title, t.Description, t.Deadline, t.Id)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 }
 
 /* Mark a Task as completed by its id */
-func completeTask(id int, client *gin.Context, cancel context.CancelFunc) {
-	c := connectDB(client, cancel)
+func completeTask(id int, client *gin.Context) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	_, err := c.Exec(context.Background(), "UPDATE tasks SET completed='t' WHERE id=$1;", id);
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 }
 
 /* Mark a previously completed task as incomplete by its id */
-func incompleteTask(id int, client *gin.Context, cancel context.CancelFunc) {
-	c := connectDB(client, cancel)
+func incompleteTask(id int, client *gin.Context) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	_, err := c.Exec(context.Background(), "UPDATE tasks SET completed='f' WHERE id=$1;", id);
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 }
 
 /* Deletes a Task in the database with the corresponding id */
-func deleteTask(id int, client *gin.Context, cancel context.CancelFunc) {
-	c := connectDB(client, cancel)
+func deleteTask(id int, client *gin.Context) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	// use Exec to execute a query that does not return a result set
 	commandTag, err := c.Exec(context.Background(), "DELETE FROM tasks where id=$1;", id)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 	if commandTag.RowsAffected() != 1 {
 		fmt.Fprintf(os.Stderr, "No row found to delete\n")
-		client.JSON(500, gin.H{"error": err.Error()})
+		client.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
 		return;
 	}
 }
 
 /* Adds a Task to the database */
-func addTask(params CreateTaskParams, client *gin.Context, cancel context.CancelFunc) {
-	c := connectDB(client, cancel)
+func addTask(params CreateTaskParams, client *gin.Context) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	commandTag, err := c.Exec(context.Background(), "INSERT INTO tasks (category_id, title, description, deadline, user_id, completed) VALUES ($1, $2, $3, $4, $5, FALSE);", params.Category_Id, params.Title, params.Description, params.Deadline, params.User.Id)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 	if commandTag.RowsAffected() != 1 {
 		fmt.Fprintf(os.Stderr, "Task not added to db\n")
-		client.JSON(500, gin.H{"error": err.Error()})
+		client.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
 		return;
 	}
 }
 
 /* Adds a Category to the database */
-func addCategory(params CreateCategoryParams, client *gin.Context, cancel context.CancelFunc) {
-	c := connectDB(client, cancel)
+func addCategory(params CreateCategoryParams, client *gin.Context) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	commandTag, err := c.Exec(context.Background(), "INSERT INTO categories (user_id, title) VALUES ($1, $2);", params.User.Id, params.Title)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 	if commandTag.RowsAffected() != 1 {
 		fmt.Fprintf(os.Stderr, "Category not added to db\n")
-		client.JSON(500, gin.H{"error": err.Error()})
+		client.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
 		return;
 	}
 }
 
 /* Returns a list of categories with their associated primary-keys */
-func getAllCategories(user_id int, client *gin.Context, cancel context.CancelFunc) ([]Category) {
-	c := connectDB(client, cancel)
+func getAllCategories(user_id int, client *gin.Context) ([]Category) {
+	c := connectDB(client)
 	defer c.Close(context.Background())
 
 	categories, err := c.Query(context.Background(), "SELECT id, title from categories WHERE user_id=$1 ORDER BY id;", user_id)
-	assertDBOperationSuccess(client, cancel, err);
+	assertDBOperationSuccess(client, err);
 	defer categories.Close();
 
 	var categorySlice []Category
@@ -509,7 +482,7 @@ func getAllCategories(user_id int, client *gin.Context, cancel context.CancelFun
 			&cat.Id,
 			&cat.Title,
 		)
-		assertDBOperationSuccess(client, cancel, err);
+		assertDBOperationSuccess(client, err);
 		categorySlice = append(categorySlice, cat)
 	}
 
@@ -519,45 +492,41 @@ func getAllCategories(user_id int, client *gin.Context, cancel context.CancelFun
 /* ------------------------------------------------------------ HELPER FUNCTIONS --------------------- */
 // checks if there is an error connecting to the database,
 //		if so, returns an error message to the client and cancels the context of the caller
-func assertDBSuccess(client *gin.Context, cancel context.CancelFunc, e error) {
+func assertDBSuccess(client *gin.Context, e error) {
 	if (e != nil) {
 		// print error message on server side so that its visible in the server logs
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", e);
 
 		// return http code of 500 to the client, which stands for "Internal Server Error"
-		client.JSON(500, gin.H{"error": e.Error()});
+		client.AbortWithStatusJSON(500, gin.H{"error": e.Error()});
 
 		// halts execution of remaining functions to not do unnecessary work
-		cancel();
+		
 	}
 }
 
 // checks if there is an error performing the specified request on the database,
 //		if so, returns an error message to the client and cancels the context of the caller
-func assertDBOperationSuccess(client *gin.Context, cancel context.CancelFunc, e error) {
+func assertDBOperationSuccess(client *gin.Context, e error) {
 	if (e != nil) {
 		// print error message on server side so that its visible in the server logs
 		fmt.Fprintf(os.Stderr, "Unable to perform the requested action: %v\n", e);
 
 		// return http code of 500 to the client, which stands for "Internal Server Error"
-		client.JSON(500, gin.H{"error": e.Error()});
-
-		// halts execution of remaining functions to not do unnecessary work
-		cancel();
+		//		and halts execution of remaining functions to not do unnecessary work
+		client.AbortWithStatusJSON(500, gin.H{"error": e.Error()});		
 	}
 }
 
 // checks if there is an error connecting to the parsing JSON body,
 //		if so, returns an error message to the client and stops execution of any remaining function-calls
-func assertJSONSuccess(client *gin.Context, cancel context.CancelFunc, e error) {
+func assertJSONSuccess(client *gin.Context, e error) {
 	if (e != nil) {
 		// print error message on server side so that its visible in the server logs
 		fmt.Fprintf(os.Stderr, "Unable to parse JSON body: %v\n", e);
 
-		// return http code of 406 to the client, which stands for "Not Acceptable"
-		client.JSON(406, gin.H{"error": e.Error()});
-
-		// halts execution of remaining functions to not do unnecessary work
-		cancel();
+		// return http code of 406 to the client, which stands for "Not Acceptable" and
+		// 		halts execution of remaining functions to not do unnecessary work		
+		client.AbortWithStatusJSON(406, gin.H{"error": e.Error()});		
 	}
 }
